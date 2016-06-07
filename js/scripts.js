61708a6b143b278dffabbdeabb8bdd3ce15ffeb1
Bang Nguyen and Georgina Van Dort <bang@>
2016-06-07 09:39:22 -0700

    use for loop to display the cream flavor

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
 var favoriteFlavors = ['oreo', 'strawberry', 'vanilla', 'rum-raisin'];
- favoriteFlavors.forEach(function(favoriteFlavor) {
-    $("#list").text(favoriteFlavors);
-  });
-  });
+  for (var i = 0; i < favoriteFlavors.length; i += 1) {
+    $(".favoriteFlavor").append("<li>" + favoriteFlavors[i] + "</li>");
+    }
+  })
+ //
+ // favoriteFlavors.forEach(function(favoriteFlavor) {
+ //    $("#list").text(favoriteFlavors);
+ //  });
+ //  });
   // $(document).ready(function() {
   //   $("#blanks form").submit(function(event) {
   //     var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
